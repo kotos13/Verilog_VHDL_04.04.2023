4c95beaf2fe1d953b2a6ebdc6f001552cf5d9b77
kotos13 <koshara-kostya@tut.by>
2023-04-04 17:57:52 +0300

    some comments

--- CPLD_Verilog/hdl/verilog/CPLD_CONTROL.v
+++ CPLD_Verilog/hdl/verilog/CPLD_CONTROL.v
@@ -1,13 +1,13 @@
-/*--------------------------------------------------------------------------------
-��������:				������ �������� ���������� � ����������� ��� �����
-�������� ������:		������� �������������� ���� ���������� �������� (�������� ������� ������������ �������) � ����������� ����� ������� ������ �����������.
-						� ���������� ������ ������ �������� �������������� ��������� ������� � FPGA_PROG_B, � ������ ����������� LED_CPLD ����� ��� ������
-						����������. � ������ ������ ����������� �������������� ��������� ������� � FPGA_PROG_B, � ������ ����������� LED_CPLD ���������� ����
-						�� ���� ������� ������, ��������������� ���� ������ (�� �������� ������������ ������� ��� ��-�� ���������� ����������� ���������� �����������),
-						� �����, � ������ ������ �� �������� ������������ �������, ����������� �������� �������� ���������� ����, �������������� ���������� ������
-						����������� �������.
-						�������� ������� ���������� ������ �������� ��������� � ��������� ~1,68 � ����� ��� ���������.
---------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------
+-- Название:				модуль контроля напряжений и температуры для МЦФОС
+-- Описание работы:			модулем контролируется шина логических сигналов (выходные сигналы супервизоров питания) и температура через внешний датчик температуры.
+--							В нормальном режиме работы включены контролируемые источники питания и FPGA_PROG_B, в группе светодиодов LED_CPLD горят два зелёных
+--							светодиода. В случае аварии отключаются контролируемые источники питания и FPGA_PROG_B, в группе светодиодов LED_CPLD загорается один
+--							из двух красных диодов, соответствующий типу аварии (по сигналам супервизоров питания или из-за превышения максимально допустимой температуры),
+--							а также, в случае аварии по сигналам супервизоров питания, посредством диодного драйвера зажигается диод, соответствующй вызвавшему аварию
+--							супервизору питания.
+--							Контроль входных параметров модуль начинает выполнять с задержкой ~1,68 с после его включения.
+----------------------------------------------------------------------------------*/
 
 
 `timescale 1 ns / 1 ps
@@ -17,34 +17,36 @@ module CPLD_CONTROL #(
 	parameter C_LEDS_QUANTITY = 24,
 	parameter C_TEMP_SENSOR_PO_WL = 16,
 	parameter C_TEMPERATURE_DATA_WL = 13,
-	parameter C_MAX_TEMP = 60.0
+	parameter C_MAX_TEMP = 60.0									//порог температуры, град Цельсия (диапазон от -55 до +150)
 )(
 	input wire CLK_IN,
-	input wire [(C_LEDS_QUANTITY+1-1):0]PGOOD_All_bus_IN,
-	input wire [3:0]KEY_CPLD_IN,
+	input wire [(C_LEDS_QUANTITY+1-1):0]PGOOD_All_bus_IN,		//'1' - ok, '0' - fail
+	input wire [3:0]KEY_CPLD_IN,								//'1' - unpressed, '0' - pressed
 		
-	input wire Temp_sensor_SO_IN,
-	output wire EN_ANALOG_PWR_OUT,
-	output wire EN_DIGITAL_3V3_OUT,
-	output wire EN_5V0_POWER_OUT,
-	output wire EN_VCC_2V5_OUT,
-	output wire EN_VCC_1V0_INT_OUT,
+	input wire Temp_sensor_SO_IN,								//temperature sensor output data line (serial)
+	
+	output wire EN_ANALOG_PWR_OUT,								//'0' - off, '1' - on
+	output wire EN_DIGITAL_3V3_OUT,								//'0' - off, '1' - on
+	output wire EN_5V0_POWER_OUT,								//'0' - off, '1' - on
+	output wire EN_VCC_2V5_OUT,									//'0' - off, '1' - on
+	output wire EN_VCC_1V0_INT_OUT,								//'0' - off, '1' - on
 
 	output wire LED_CPLD_0_RED_OUT,
 	output wire LED_CPLD_1_GREEN_OUT,
 	output wire LED_CPLD_2_RED_OUT,
-	output wire LED_CPLD_3_GREEN_OUT,
+	output wire LED_CPLD_3_GREEN_OUT,							//'1' - off, '0' - on
 
-	output wire LED_DRIVER_CLK_OUT,
-	output reg LED_DRIVER_SDI_OUT = 1'b0,
-	output wire LED_DRIVER_LE_OUT,
+	output wire LED_DRIVER_CLK_OUT,								//led driver clock
+	output reg LED_DRIVER_SDI_OUT = 1'b0,						//led driver input data line (serial)
+	output wire LED_DRIVER_LE_OUT,								//led driver latch enable
 
-	output wire Temp_sensor_CS_OUT,
-	output wire Temp_sensor_SCK_OUT,
-	output wire FPGA_PROG_B_OUT
+	output wire Temp_sensor_CS_OUT,								//temperature sensor chip select
+	output wire Temp_sensor_SCK_OUT,							//temperature sensor clock
+	output wire FPGA_PROG_B_OUT									//'0' - reset FPGA
 );
 
-parameter [C_TEMPERATURE_DATA_WL-1:0]MAX_TEMP_CODE_CONST = {{C_TEMPERATURE_DATA_WL{1'b0}} + (C_MAX_TEMP / 0.0625)};
+//Constant declaration
+parameter [C_TEMPERATURE_DATA_WL-1:0] MAX_TEMP_CODE_CONST = {{C_TEMPERATURE_DATA_WL{1'b0}} + (C_MAX_TEMP / 0.0625)};
 parameter FILTER_LENGTH = 3;
 
 parameter Stop_state = 2'b00;
@@ -57,6 +59,7 @@ parameter Strobe_state3 = 2'b10;
 
 reg TX_curr_mode = Start_state;
 reg RX_curr_mode = Stop_state;
+
 reg GSR_signal;
 reg CLK_signal;
 reg [26:0]CLK_COUNTER_signal = 1'b0;
@@ -92,7 +95,7 @@ always @* begin
 	CLK_signal = CLK_IN; 
 end
 
-
+//добавлено в демонстрационных целях, в реальном проекте отсутствует (несинтезируемая языковая конструкция)
 always @(posedge GSR_signal) begin
 	GSR_signal <= 1'b1;
 	#100;
@@ -100,6 +103,7 @@ always @(posedge GSR_signal) begin
 	#0;
 end
 
+//Clock counter
 always @(posedge CLK_signal or posedge GSR_signal) begin
 	if (GSR_signal == 1'b1) begin
 		assign CLK_COUNTER_signal = CLK_signal;
@@ -111,7 +115,7 @@ end
 always @* begin
 	CLK_DIV_TX = CLK_COUNTER_signal[1];
 	CLK_DIV_RX = CLK_COUNTER_signal[3];
-	CLK_DIV_filter = CLK_COUNTER_signal[2];
+	CLK_DIV_filter = CLK_COUNTER_signal[2];							//period 0.2 us (при f = 40 МГц delay = 1/(40e6)*2*2^2)
 end
 
 if (C_SIMULATION) begin : GEN_SIMULATION
@@ -119,12 +123,12 @@ if (C_SIMULATION) begin : GEN_SIMULATION
 		if (GSR_signal == 1'b1) begin
 			START_CONTROL_TIME_signal <= 1'b0;
 		end if (CLK_COUNTER_signal[7] == 1'b1) begin
-			START_CONTROL_TIME_signal <= 1'b1;
+			START_CONTROL_TIME_signal <= 1'b1;						//delay 3.2 us (при f = 40 МГц delay = 1/(40e6)*2^7)
 		end
 	end
 
 	always @* begin 
-		RX_period_signal = CLK_COUNTER_signal[8];
+		RX_period_signal = CLK_COUNTER_signal[8];					//period 12.8 us (при f = 40 МГц delay = 1/(40e6)*2*2^8)
 	end
 end
 
@@ -133,12 +137,12 @@ if (!C_SIMULATION) begin : GEN_SIMULATION_NONE
 		if (GSR_signal == 1'b1) begin
 			START_CONTROL_TIME_signal <= 1'b0;
 		end if (CLK_COUNTER_signal[26] == 1'b1) begin
-			START_CONTROL_TIME_signal <= 1'b1;
+			START_CONTROL_TIME_signal <= 1'b1;						//delay 1.68 s (при f = 40 МГц delay = 1/(40e6)*2^26)
 		end
 	end
 
 	always @* begin
-		RX_period_signal = CLK_COUNTER_signal[24];
+		RX_period_signal = CLK_COUNTER_signal[24];					//period 0.84 s (при f = 40 МГц delay = 1/(40e6)*2*2^24)
 	end
 end
 
@@ -148,8 +152,7 @@ always @* begin
 	PGOOD_All_bus_signal[15:0] = PGOOD_All_bus_IN[15:0];
 end
 
-
-
+//PGOOD_filtered shift register
 always @(posedge GSR_signal or posedge CLK_DIV_filter) begin
 	if (GSR_signal == 1'b1) begin
 		PGOOD_filtered_signal[0] <= 1'b1;
@@ -187,6 +190,7 @@ always @* begin
 	CRASH_signal = CRASH_POWER_TRIG_signal || CRASH_TEMP_TRIG_signal;
 end
 
+//Защёлкивание входных данных от супервизоров
 always @(posedge GSR_signal or posedge CRASH_POWER_TRIG_signal) begin
 	if (GSR_signal == 1'b1) begin
 		assign SDI_Data_signal = GSR_signal;
@@ -195,6 +199,7 @@ always @(posedge GSR_signal or posedge CRASH_POWER_TRIG_signal) begin
 	end
 end
 
+// Управление включением питания
 always @(posedge GSR_signal or posedge CLK_signal) begin
 	if (GSR_signal == 1'b1) begin
 		ENABLE_ALL_POWER_INV_signal <= 1'b0;
@@ -219,14 +224,15 @@ assign LED_CPLD_2_RED_OUT = KEY_CPLD_IN[2] && !CRASH_POWER_TRIG_signal;
 assign LED_CPLD_3_GREEN_OUT = KEY_CPLD_IN[3] && CRASH_POWER_TRIG_signal;
 
 reg Data_serial_counter_TX = 0;
+//Работа передатчика. Вывод данных на выход LED_DRIVER_SDI_OUT
 always @(posedge GSR_signal or posedge CLK_DIV_TX) begin
 	if (GSR_signal == 1'b1) begin
 		LED_DRIVER_SDI_OUT <= 1'b0;
 		LED_DRIVER_LE_signal <= 1'b0;
 		TX_sinchro_signal <= 1'b0;
 		Data_serial_counter_TX = C_LEDS_QUANTITY;
-	end if (CLK_DIV_TX == 1'b0) begin
-		if (TX_curr_mode == Run_state) begin /*РАЗОБРАТЬ строку 245*/
+	end if (CLK_DIV_TX == 1'b0) begin									//синхронизация по заднему фронту CLK_DIV_TX
+		if (TX_curr_mode == Run_state) begin
 			if (Data_serial_counter_TX != 0) begin
 				assign LED_DRIVER_SDI_OUT = SDI_Data_signal[Data_serial_counter_TX-1];
 				TX_sinchro_signal <= 1'b1;
@@ -246,6 +252,7 @@ end
 assign LED_DRIVER_CLK_OUT = CLK_DIV_TX & TX_sinchro_signal;
 assign LED_DRIVER_LE_OUT = LED_DRIVER_LE_signal;
 
+//Конечный автомат управления передатчиком
 always @(posedge GSR_signal or posedge CLK_DIV_TX) begin
 	if (GSR_signal == 1'b1) begin
 		assign TX_curr_mode = Start_state;
@@ -281,6 +288,7 @@ always @(posedge GSR_signal or posedge CLK_DIV_TX) begin
 	end
 end	
 
+//Приёмник датчика температуры
 always @(posedge GSR_signal or posedge CLK_signal) begin
 	if (GSR_signal == 1'b1) begin 
 		CRASH_TEMP_TRIG_signal <= 1'b0;
@@ -292,6 +300,7 @@ always @(posedge GSR_signal or posedge CLK_signal) begin
 end
 
 reg Data_serial_counter_RX = 0;
+//Работа приёмника. Ввод данных на вход Temp_sensor_SO_IN
 always @(posedge GSR_signal or posedge CLK_DIV_RX) begin 
 	if (GSR_signal == 1'b1) begin
 		assign Temp_sensor_CS_signal = 1'b1;
@@ -321,6 +330,7 @@ end
 assign Temp_sensor_SCK_OUT = Temp_sensor_SCK_signal;
 assign Temp_sensor_CS_OUT = Temp_sensor_CS_signal;
 
+//Temp_sensor_PO shift register
 always @(posedge GSR_signal or posedge Temp_sensor_SCK_signal) begin 
 	if (GSR_signal == 1'b1) begin
 		Temp_sensor_PO_signal <= 1'b0;
@@ -341,6 +351,7 @@ generate
 	end
 endgenerate	
 
+//Конечный автомат управления стробом периода считывания данных с датчика температуры
 always @(posedge GSR_signal or posedge CLK_signal) begin
 	if (GSR_signal == 1'b1) begin
 		assign Strobe_curr_state = Strobe_state1;
@@ -377,6 +388,7 @@ always @* begin
 	RX_period_strobe_signal = (Strobe_curr_state == Strobe_state2) ? 1'b1 : 1'b0;
 end
 
+//Конечный автомат управления приёмником
 always @(posedge GSR_signal or posedge CLK_DIV_RX) begin
 	assign RX_curr_mode = Stop_state;
 	if (CLK_DIV_RX == 1'b1) begin
@@ -408,26 +420,3 @@ always @(posedge GSR_signal or posedge CLK_DIV_RX) begin
 end
 
 endmodule
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
